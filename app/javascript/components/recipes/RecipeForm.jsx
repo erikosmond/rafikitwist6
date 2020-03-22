6ff41e012e49e9afd8746c61f814455cd50bdcc6
Erik Osmond <erikosmond@gmail.com>
2020-03-22 14:02:13 -0400

    add fields for ingredients

--- app/javascript/components/recipes/RecipeForm.jsx
+++ app/javascript/components/recipes/RecipeForm.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 // import Multiselect from 'react-widgets/lib/Multiselect'
-// import SelectList from 'react-widgets/lib/SelectList'
-// import DropdownList from 'react-widgets/lib/DropdownList'
 import { Field, reduxForm } from 'redux-form'
 import RecipeFormStyles from 'components/styled/RecipeFormStyles'
 import RecipeFormIngredient from 'components/recipes/RecipeFormIngredient'
@@ -13,42 +11,59 @@ const styles = () => (RecipeFormStyles)
 let RecipeForm = (props) => {
   const { classes, handleSubmit, ingredientOptions } = props
 
-  const colors = [ { color: 'Red', value: 'ff0000' },
-  { color: 'Green', value: '00ff00' },
-  { color: 'Blue', value: '0000ff' } ]
-
   return (
     <form onSubmit={handleSubmit}>
       <div className={classes.container}>
         <div className={classes.nameLabel} htmlFor="recipeName">Name</div>
         <Field className={classes.nameField} name="recipeName" component="input" type="text" />
-        <label className={classes.descriptionLabel} htmlFor="description">Description</label>
-        <Field className={classes.descriptionField} name="description" component="input" type="text" />
-        <label className={classes.instructionsLabel} htmlFor="instructions">Instructions</label>
-        <Field className={classes.instructionsField} name="instructions" component="input" type="text" />
+        <div className={classes.descriptionLabel} htmlFor="description">Description</div>
+        <Field
+          className={classes.descriptionField}
+          name="description"
+          component="input"
+          type="text"
+        />
+        <div className={classes.instructionsLabel} htmlFor="instructions">Instructions</div>
+        <Field
+          className={classes.instructionsField}
+          name="instructions"
+          component="input"
+          type="text"
+        />
       </div>
       {/* TODO: figure out how to pass a blank object into field values */}
-      <Field
-        name="ingredient1"
-        component={RecipeFormIngredient}
-        props={{ ingredientOptions, placeholder: 'Ingredient' }}
-        defaultValue={{}}
-        // eslint-disable-next-line react/jsx-no-bind
-        format={(value) => value === '' ? {} : value}
-      />
-      {/* <RecipeFormIngredient
-        ingredientOptions={ingredientOptions}
-      /> */}
-      {/* <div>
-        <label>Favorite Color</label>
-        <Field
-          name="favoriteColor"
-          component={DropdownList}
-          // data={objectToList(ingredientOptions)}
-          data={ingredientOptions}
-          valueField="value"
-          textField="color"/>
-      </div>
+      {[...Array(1).keys()].map((k) => (
+        <div key={k}>
+          <Field
+            name={`ingredientAmount-${k}`}
+            component="input"
+            type="text"
+            placeholder="amount"
+          />
+          <Field
+            name={`ingredientMod-${k}`}
+            component="input"
+            type="text"
+            placeholder="will be dropdown"
+          />
+          <Field
+            name={`ingredient-${k}`}
+            component={RecipeFormIngredient}
+            props={{ ingredientOptions, placeholder: 'Ingredient' }}
+            defaultValue={{}}
+            // eslint-disable-next-line react/jsx-no-bind
+            format={(value) => value === '' ? {} : value}
+          />
+          <Field
+            name={`ingredientPrep-${k}`}
+            component="input"
+            type="text"
+            placeholder="E.g. diced, cubed, crushed, etc."
+          />
+          <br />
+        </div>
+      ))}
+      {/*
       <div>
         <label>Hobbies</label>
         <Field
@@ -58,14 +73,7 @@ let RecipeForm = (props) => {
           onBlur={() => props.onBlur()}
           data={[ 'Guitar', 'Cycling', 'Hiking' ]}/>
       </div>
-      <div>
-        <label>Sex</label>
-        <Field
-          name="sex"
-          component={SelectList}
-          onBlur={() => props.onBlur()}
-          data={[ 'male', 'female' ]}/>
-      </div> */}
+      */}
 
       <button type="submit">Save</button>
     </form>
@@ -73,7 +81,6 @@ let RecipeForm = (props) => {
 }
 
 RecipeForm = reduxForm({
-  // a unique name for the form
   form: 'recipeForm',
 })(RecipeForm)
 
@@ -82,7 +89,17 @@ RecipeForm.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
   })),
-  classes: PropTypes.shape({}),
+  classes: PropTypes.shape({
+    container: PropTypes.string,
+    nameLabel: PropTypes.string,
+    nameField: PropTypes.string,
+    descriptionLabel: PropTypes.string,
+    descriptionField: PropTypes.string,
+    instructionsField: PropTypes.string,
+    instructionsLabel: PropTypes.string,
+  }),
+  // eslint-disable-next-line react/require-default-props
+  handleSubmit: PropTypes.func,
 }
 
 RecipeForm.defaultProps = {
@@ -91,4 +108,3 @@ RecipeForm.defaultProps = {
 }
 
 export default withStyles(styles)(RecipeForm)
-// export default RecipeForm
