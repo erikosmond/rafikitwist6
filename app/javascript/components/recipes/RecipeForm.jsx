55fa8acb9634420509f233fffcf90ae836899ace
Erik Osmond <erikosmond@gmail.com>
2020-03-29 22:50:47 -0400

    basic field array example working

--- app/javascript/components/recipes/RecipeForm.jsx
+++ app/javascript/components/recipes/RecipeForm.jsx
@@ -2,18 +2,70 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 // import Multiselect from 'react-widgets/lib/Multiselect'
-import { Field, reduxForm } from 'redux-form'
+import { Field, FieldArray, reduxForm } from 'redux-form'
 import RecipeFormStyles from 'components/styled/RecipeFormStyles'
 import RecipeFormIngredient from 'components/recipes/RecipeFormIngredient'
 
 const styles = () => (RecipeFormStyles)
 
+const renderField = (args) => {
+  const {
+    input, label, type, meta: { touched, error }
+  } = args
+  return (
+    <div>
+      <label>{label}</label>
+      <div>
+        <input {...input} type={type} placeholder={label} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+  )
+}
+
+const renderIngredients = (args) => {
+  const { fields, meta: { error, submitFailed } } = args
+  return (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push({})}>
+          Add Ingredient
+        </button>
+        {submitFailed && error && <span>{error}</span>}
+      </li>
+      {fields.map((member, index) => (
+        <li key={index}>
+          <button
+            type="button"
+            title="Remove Member"
+            onClick={() => fields.remove(index)}
+          />
+          <h4>Member #{index + 1}</h4>
+          <Field
+            name={`${member}.firstName`}
+            type="text"
+            component={renderField}
+            label="First Name"
+          />
+          <Field
+            name={`${member}.lastName`}
+            type="text"
+            component={renderField}
+            label="Last Name"
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 let RecipeForm = (props) => {
   const { classes, handleSubmit, ingredientOptions } = props
+
   return (
     <form onSubmit={handleSubmit}>
       <div className={classes.container}>
-        <div className={classes.nameLabel} htmlFor="recipeName">Name</div>
+        <div className={classes.nameLabel} htmlFor="recipeName">Recipe Name</div>
         <Field className={classes.nameField} name="recipeName" component="input" type="text" />
         <div className={classes.descriptionLabel} htmlFor="description">Description</div>
         <Field
@@ -30,7 +82,8 @@ let RecipeForm = (props) => {
           type="text"
         />
       </div>
-      {[...Array(1).keys()].map((k) => (
+      <FieldArray name="ingredients" component={renderIngredients} />
+      {/* {[...Array(1).keys()].map((k) => (
         <div key={k}>
           <Field
             name={`ingredientAmount-${k}`}
@@ -60,7 +113,7 @@ let RecipeForm = (props) => {
           />
           <br />
         </div>
-      ))}
+      ))} */}
       {/*
       <div>
         <label>Hobbies</label>
