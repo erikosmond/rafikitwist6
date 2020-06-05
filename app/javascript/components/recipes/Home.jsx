f0f71af07efc7a5022685c4b8e2528562a382ac4
Erik Osmond <erikosmond@gmail.com>
2020-06-05 07:00:46 -0400

    add adaptive design foundation

--- app/javascript/components/recipes/Home.jsx
+++ app/javascript/components/recipes/Home.jsx
@@ -1,58 +1,28 @@
 /* eslint-disable react/jsx-no-bind */
 
 import React from 'react'
+import Media from 'react-media'
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom'
-
-import styled from 'styled-components'
-
-import CommentModal from 'containers/CommentModalContainer'
-import TagFormModal from 'containers/TagFormContainer'
-import RecipeSkeleton from 'containers/RecipeContainer'
-import RecipeFormSkeleton from 'containers/RecipeFormContainer'
-import RecipeList from 'containers/RecipeListContainer'
-import RecipeHeader from 'containers/RecipeHeaderContainer'
-
-const StyledContent = styled.div`
-  margin-top: 70px;
-`
+import LargeHome from 'components/recipes/LargeHome'
+import MolbileHome from 'components/recipes/MobileHome'
 
 const Home = () => (
-  <Router>
-    <div>
-      <RecipeHeader />
-      <CommentModal />
-      <TagFormModal />
-      <StyledContent>
-        <Switch>
-          <Route
-            path="/tags/:tagId/recipes"
-            component={RecipeList}
-          />
-          <Route
-            path="/recipes/new"
-            component={RecipeFormSkeleton}
-          />
-          <Route
-            path="/recipes/:recipeId/edit"
-            render={(props) => <RecipeFormSkeleton {...props} edit />}
-          />
-          <Route
-            path="/recipes/:recipeId"
-            component={RecipeSkeleton}
-          />
-          <Route
-            path="/"
-            component={RecipeList}
-          />
-        </Switch>
-      </StyledContent>
-    </div>
-  </Router>
+  <div>
+    <Media queries={{
+      small: '(max-width: 599px)',
+      medium: '(min-width: 600px) and (max-width: 1199px)',
+      large: '(min-width: 1200px)',
+    }}
+    >
+      {(matches) => (
+        <>
+          {matches.small && <MolbileHome />}
+          {matches.medium && <p>I am medium!</p>}
+          {matches.large && <LargeHome />}
+        </>
+      )}
+    </Media>
+  </div>
 )
 
 export default Home
---
+++ app/javascript/components/recipes/LargeHome.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/jsx-no-bind */
+
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+
+import styled from 'styled-components'
+
+import CommentModal from 'containers/CommentModalContainer'
+import TagFormModal from 'containers/TagFormContainer'
+import RecipeSkeleton from 'containers/RecipeContainer'
+import RecipeFormSkeleton from 'containers/RecipeFormContainer'
+import RecipeList from 'containers/RecipeListContainer'
+import RecipeHeader from 'containers/RecipeHeaderContainer'
+
+const StyledContent = styled.div`
+  margin-top: 70px;
+`
+
+const LargeHome = () => (
+  <Router>
+    <RecipeHeader />
+    <CommentModal />
+    <TagFormModal />
+
+    <StyledContent>
+      <Switch>
+        <Route
+          path="/tags/:tagId/recipes"
+          component={RecipeList}
+        />
+        <Route
+          path="/recipes/new"
+          component={RecipeFormSkeleton}
+        />
+        <Route
+          path="/recipes/:recipeId/edit"
+          render={(props) => <RecipeFormSkeleton {...props} edit />}
+        />
+        <Route
+          path="/recipes/:recipeId"
+          component={RecipeSkeleton}
+        />
+        <Route
+          path="/"
+          component={RecipeList}
+        />
+      </Switch>
+    </StyledContent> 
+  </Router>
+)
+
+export default LargeHome
