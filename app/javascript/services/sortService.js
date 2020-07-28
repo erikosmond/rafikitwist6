c007f406513aef064b2464dbe2da915f9ff0d3b9
Erik Osmond <erikosmond@gmail.com>
2020-07-28 16:07:38 -0400

    add priority sorting

--- app/javascript/services/sortService.js
+++ app/javascript/services/sortService.js
@@ -1,8 +1,16 @@
+const compareTagName = (allTags) => (a, b) => compare(allTags[a], allTags[b])
+
+const priotiesRanks = {
+  'On Deck': 5,
+  'Highest priority': 4,
+  'High priority': 3,
+  'Medium priority': 2,
+  'Low priority': 1,
+}
+
 export function sortByName(array) {
   return array.sort(compareName)
 }
-
-const compareTagName = (allTags) => (a, b) => compare(allTags[a], allTags[b])
 
 export function sortByTagName(array, allTags) {
   return array.sort(compareTagName(allTags))
@@ -18,6 +26,13 @@ function compareRatingThenName(a, b) {
   if (a.ratings && b.ratings) {
     if (a.ratings[0].tagName > b.ratings[0].tagName) return -1
     if (a.ratings[0].tagName < b.ratings[0].tagName) return 1
+  }
+
+  if (a.priorities && !b.priorities) return -1
+  if (!a.priorities && b.priorities) return 1
+  if (a.priorities && b.priorities) {
+    if (priotiesRanks[a.priorities[0].tagName] > priotiesRanks[b.priorities[0].tagName]) return -1
+    if (priotiesRanks[a.priorities[0].tagName] < priotiesRanks[b.priorities[0].tagName]) return 1
   }
 
   if (a.name > b.name) return 1
